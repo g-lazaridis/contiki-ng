12ffc1b7294b4b9a79594a46d9b146591c65a323
Gabriel Lazaridis <glazaridis@f-in.eu>
2024-06-28 17:26:39 +0300

    lr1110 files
    Signed-off-by: Gabriel Lazaridis <glazaridis@f-in.eu>

--- arch/cpu/nrf52840/dev/lr11xx_hal.c
+++ arch/cpu/nrf52840/dev/lr11xx_hal.c
@@ -1,9 +1,9 @@
 /**
- * @file lr11xx_spi_arch.c
+ * @file lr11xx_hal.c
  * @author glazaridis (glazaridis@f-in.eu)
  * @brief
  * @version 0.1
- * @date 2024-06-26
+ * @date 2024-06-27
  *
  * @copyright Copyright (c) 2024
  *
