2763cb38b54b887b6cbcaabbf77862884aab4371
galazari <gabriel.laz@hotmail.com>
2024-06-30 23:28:35 +0300

    hal
    Signed-off-by: galazari <gabriel.laz@hotmail.com>

--- arch/cpu/nrf52840/dev/lr11xx_hal.c
+++ arch/cpu/nrf52840/dev/lr11xx_hal.c
@@ -8,3 +8,17 @@
  * @copyright Copyright (c) 2024
  *
  */
+
+#include "lr11xx_hal.h"
+/*---------------------------------------------------------------------------*/
+static int
+hal_spi_init(void)
+{
+}
+/*---------------------------------------------------------------------------*/
+lr11xx_hal_status_t
+lr11xx_hal_write(const void *context, const uint8_t *command, const uint16_t command_length,
+                 const uint8_t *data, const uint16_t data_length)
+{
+}
+/*---------------------------------------------------------------------------*/
