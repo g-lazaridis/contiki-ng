709d270556958e7d18700b1385452cb497ace4ac
Gabriel Lazaridis <glazaridis@f-in.eu>
2024-07-02 18:10:20 +0300

    rearange
    Signed-off-by: Gabriel Lazaridis <glazaridis@f-in.eu>

--- arch/cpu/nrf52840/dev/lr11xx_hal.c
+++ arch/cpu/nrf52840/dev/lr11xx_hal.c
@@ -16,35 +16,9 @@
 #define LOG_MODULE "LR11XX-HAL"
 #define LOG_LEVEL LOG_LEVEL_INFO
 /*---------------------------------------------------------------------------*/
-#define LR11XX_SPI_INSTANCE     0
-static const nrf_drv_spi_t spi = NRF_DRV_SPI_INSTANCE(LR11XX_SPI_INSTANCE);  /**< SPI instance. */
-static bool spi_initialized = false;
-/*---------------------------------------------------------------------------*/
-static int
-hal_spi_init(void)
-{
-  nrf_drv_spi_config_t spi_config = NRF_DRV_SPI_DEFAULT_CONFIG;
-  spi_config.ss_pin = LR1110_SPI_CS;
-  spi_config.miso_pin = LR1110_SPI_MISO;
-  spi_config.mosi_pin = LR1110_SPI_MOSI;
-  spi_config.sck_pin = LR1110_SPI_SCK;
-  spi_config.orc = 0x00;
-
-  return nrf_drv_spi_init(&spi, &spi_config, NULL, NULL) == NRF_SUCCESS ? 0 : -1;
-}
-/*---------------------------------------------------------------------------*/
 lr11xx_hal_status_t
 lr11xx_hal_write(const void *context, const uint8_t *command, const uint16_t command_length,
                  const uint8_t *data, const uint16_t data_length)
 {
-  int ret;
-  if(!spi_initialized) {
-    ret = hal_spi_init();
-    if(ret) {
-      LOG_ERROR("lr11xx_hal_write: Failed to initialize SPI\n");
-      return LR11XX_HAL_STATUS_ERROR;
-    }
-    spi_initialized = true;
-  }
 }
 /*---------------------------------------------------------------------------*/
---
+++ arch/dev/radio/lr1110/lr11xx-lora.c
@@ -0,0 +1,191 @@
+/**
+ * @file lr11xx-lora.c
+ * @author glazaridis (glazaridis@f-in.eu)
+ * @brief
+ * @version 0.1
+ * @date 2024-07-02
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+/*
+ * Copyright (c) 2020, Toshiba BRIL
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/*---------------------------------------------------------------------------*/
+/**
+ * \addtogroup nrf52840
+ * @{
+ *
+ * \addtogroup nrf52840-dev Device drivers
+ * @{
+ *
+ * \defgroup nrf52840-rf-ieee nRF52840 IEEE mode driver
+ *
+ * @{
+ *
+ * \file
+ * Implementation of the nRF52840 IEEE mode NETSTACK_RADIO driver
+ */
+/*---------------------------------------------------------------------------*/
+#include "contiki.h"
+#include "dev/radio.h"
+#include "sys/energest.h"
+#include "nrf_drv_spi.h"
+/*---------------------------------------------------------------------------*/
+#define LR11XX_SPI_INSTANCE     0
+static const nrf_drv_spi_t spi = NRF_DRV_SPI_INSTANCE(LR11XX_SPI_INSTANCE);  /**< SPI instance. */
+/*---------------------------------------------------------------------------*/
+static void
+enter_rx(void)
+{
+}
+/*---------------------------------------------------------------------------*/
+static int
+on(void)
+{
+  ENERGEST_ON(ENERGEST_TYPE_LISTEN);
+  return 0;
+}
+/*---------------------------------------------------------------------------*/
+static int
+channel_clear(void)
+{
+  return 0;
+}
+/*---------------------------------------------------------------------------*/
+static int
+init(void)
+{
+  ret_code_t ret;
+  nrf_drv_spi_config_t spi_config = NRF_DRV_SPI_DEFAULT_CONFIG;
+
+  spi_config.ss_pin = LR1110_SPI_CS;
+  spi_config.miso_pin = LR1110_SPI_MISO;
+  spi_config.mosi_pin = LR1110_SPI_MOSI;
+  spi_config.sck_pin = LR1110_SPI_SCK;
+  spi_config.orc = 0x00;
+
+  ret = nrf_drv_spi_init(&spi, &spi_config, NULL, NULL);
+
+  if(ret != NRF_SUCCESS) {
+    return RADIO_TX_ERR;
+  }
+
+  /*TODO, configure reset and busy gpios */
+
+  return RADIO_TX_OK;
+}
+/*---------------------------------------------------------------------------*/
+static int
+prepare(const void *payload, unsigned short payload_len)
+{
+  return RADIO_TX_OK;
+}
+/*---------------------------------------------------------------------------*/
+static int
+transmit(unsigned short transmit_len)
+{
+  return 0;
+}
+/*---------------------------------------------------------------------------*/
+static int
+send(const void *payload, unsigned short payload_len)
+{
+  return 0;
+}
+/*---------------------------------------------------------------------------*/
+static int
+read_frame(void *buf, unsigned short bufsize)
+{
+  return 0;
+}
+/*---------------------------------------------------------------------------*/
+static int
+receiving_packet(void)
+{
+  return NRF52840_RECEIVING_NO;
+}
+/*---------------------------------------------------------------------------*/
+static int
+pending_packet(void)
+{
+  return NRF52840_PENDING_NO;
+}
+/*---------------------------------------------------------------------------*/
+static int
+off(void)
+{
+  ENERGEST_OFF(ENERGEST_TYPE_LISTEN);
+
+  return NRF52840_COMMAND_OK;
+}
+/*---------------------------------------------------------------------------*/
+static radio_result_t
+get_value(radio_param_t param, radio_value_t *value)
+{
+  return RADIO_RESULT_NOT_SUPPORTED;
+}
+/*---------------------------------------------------------------------------*/
+static radio_result_t
+set_value(radio_param_t param, radio_value_t value)
+{
+  return RADIO_RESULT_NOT_SUPPORTED;
+}
+/*---------------------------------------------------------------------------*/
+static radio_result_t
+get_object(radio_param_t param, void *dest, size_t size)
+{
+  return RADIO_RESULT_NOT_SUPPORTED;
+}
+/*---------------------------------------------------------------------------*/
+static radio_result_t
+set_object(radio_param_t param, const void *src, size_t size)
+{
+  /* The radio does not support h/w frame filtering based on addresses */
+  return RADIO_RESULT_NOT_SUPPORTED;
+}
+/*---------------------------------------------------------------------------*/
+const struct radio_driver lr11xx_lora_driver = {
+  init,
+  prepare,
+  transmit,
+  send,
+  read_frame,
+  channel_clear,
+  receiving_packet,
+  pending_packet,
+  on,
+  off,
+  get_value,
+  set_value,
+  get_object,
+  set_object
+};
+/*---------------------------------------------------------------------------*/
