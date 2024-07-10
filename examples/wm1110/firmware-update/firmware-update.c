324e1bcb1b2560dcfb12e5ed4a5f48d3b11d0f36
galazari <gabriel.laz@hotmail.com>
2024-07-10 09:23:01 +0300

    firmware update experimenting
    Signed-off-by: galazari <gabriel.laz@hotmail.com>

--- examples/wm1110/firmware-update/firmware-update.c
+++ examples/wm1110/firmware-update/firmware-update.c
@@ -15,6 +15,8 @@
 #include "lr11xx_bootloader.h"
 #include "lr1110_transceiver_0401.h"
 #include "lr11xx_crypto_engine.h"
+#include "lr11xx_system.h"
+#include "lr11xx_system_types.h"
 #include "netstack.h"
 /*---------------------------------------------------------------------------*/
 /* Log configuration */
@@ -25,12 +27,27 @@
 PROCESS(playground_process, "Playground");
 AUTOSTART_PROCESSES(&playground_process);
 /*---------------------------------------------------------------------------*/
+static void
+print_status(lr11xx_bootloader_stat1_t *stat1,
+             lr11xx_bootloader_stat2_t *stat2,
+             lr11xx_bootloader_irq_mask_t *irq_status)
+{
+  LOG_INFO("Stat1:\n\tcommand status = %d\n\tis_interrupt_active = %d\n", stat1->command_status, stat1->is_interrupt_active);
+  LOG_INFO("Stat2:\n\treset status = %d\n\tchip mode = %d\n\tis running from flash = %d\n",
+           stat2->reset_status, stat2->chip_mode, stat2->is_running_from_flash);
+  LOG_INFO("Irq Status = 0x%04lX\n", *irq_status);
+}
+/*---------------------------------------------------------------------------*/
 PROCESS_THREAD(playground_process, ev, data)
 {
   static struct etimer et;
   static lr11xx_bootloader_version_t version;
-  static bool image_valid;
+  lr11xx_bootloader_stat1_t stat1;
+  lr11xx_bootloader_stat2_t stat2;
+  lr11xx_bootloader_irq_mask_t irq_status;
+  // static bool image_valid;
   lr11xx_status_t status;
+  // lr11xx_system_errors_t errors;
 
   PROCESS_BEGIN();
 
@@ -47,30 +64,74 @@ PROCESS_THREAD(playground_process, ev, data)
     LOG_ERR("Failed to get version\n");
   }
 
-  LOG_INFO("Validateing fw image...\n");
-  lr11xx_crypto_check_encrypted_firmware_image_full(NULL, 0, lr11xx_firmware_image, LR11XX_FIRMWARE_IMAGE_SIZE);
-  lr11xx_crypto_get_check_encrypted_firmware_image_result(NULL, &image_valid);
-  if(image_valid) {
-    LOG_INFO("Image validation successfull!!");
-  } else {
-    LOG_ERR("Image validation failed\n");
-  }
-  // //Reset and set to bootloader
+  lr11xx_bootloader_get_status(NULL, &stat1, &stat2, &irq_status);
+  print_status(&stat1, &stat2, &irq_status);
+
+  LOG_INFO("Clearing IRQs\n");
+  lr11xx_system_clear_irq_status(NULL, 0xFFFF);
+
+  lr11xx_bootloader_get_status(NULL, &stat1, &stat2, &irq_status);
+  print_status(&stat1, &stat2, &irq_status);
+
+  // LOG_INFO("Validateing fw image...\n");
+  // lr11xx_crypto_check_encrypted_firmware_image_full(NULL, 0, lr11xx_firmware_image, LR11XX_FIRMWARE_IMAGE_SIZE);
+  // lr11xx_crypto_get_check_encrypted_firmware_image_result(NULL, &image_valid);
+  // if(image_valid) {
+  //   LOG_INFO("Image validation successfull!!");
+  // } else {
+  //   LOG_ERR("Image validation failed\n");
+  // }
+
+  // Reset and set to bootloader
+  // LOG_INFO("Entering bootloader...\n");
   // lr11xx_bootloader_reboot(NULL, true);
   // //Wait for reboot
-  // etimer_set(&et, CLOCK_SECOND * 1);
+  // etimer_set(&et, CLOCK_SECOND);
   // PROCESS_WAIT_EVENT();
-  // //Check if in bootloader state
+  // lr11xx_bootloader_get_status(NULL, &stat1, &stat2, &irq_status);
+  // print_status(&stat1, &stat2, &irq_status);
+  // lr11xx_system_get_errors(NULL, &errors);
+  // LOG_INFO("Errors=0x%02X\n", errors);
+
+  // LOG_INFO("Resetting...\n");
+  // lr11xx_hal_reset(NULL);
+  // etimer_set(&et, CLOCK_SECOND);
+  // PROCESS_WAIT_EVENT();
+  // lr11xx_bootloader_get_status(NULL, &stat1, &stat2, &irq_status);
+  // print_status(&stat1, &stat2, &irq_status);
+  // lr11xx_system_get_errors(NULL, &errors);
+  // LOG_INFO("Errors=0x%02X\n", errors);
+  // LOG_INFO("Clearing IRQs\n");
+  // lr11xx_bootloader_clear_reset_status_info(NULL);
+  // lr11xx_bootloader_get_status(NULL, &stat1, &stat2, &irq_status);
+  // print_status(&stat1, &stat2, &irq_status);
+  // LOG_INFO("Entering bootloader...\n");
+  // lr11xx_bootloader_reboot(NULL, true);
+  // etimer_set(&et, CLOCK_SECOND);
+  // PROCESS_WAIT_EVENT();
+  // lr11xx_bootloader_get_status(NULL, &stat1, &stat2, &irq_status);
+  // print_status(&stat1, &stat2, &irq_status);
+
+  //Check if in bootloader state
   // lr11xx_bootloader_get_version(NULL, &version);
   // if(version.type == 0xdf) {
   //   LOG_INFO("Bootloader state entered!!\n");
   // } else {
   //   LOG_ERR("Failed to enter bootloader state\n");
   // }
+  // LOG_INFO("HW Version = %u\n", version.hw);
+  // LOG_INFO("FW Version = %02u.%02u\n", (uint8_t)(version.fw >> 8), (uint8_t)(version.fw));
+  // LOG_INFO("Type = %u\n", version.type);
 
   while(1) {
-    PROCESS_WAIT_EVENT();
     etimer_reset(&et);
+    PROCESS_WAIT_EVENT();
+    // lr11xx_bootloader_get_status(NULL, &stat1, &stat2, &irq_status);
+    // print_status(&stat1, &stat2, &irq_status);
+    //   lr11xx_bootloader_get_version(NULL, &version);
+    //   LOG_INFO("HW Version = %u\n", version.hw);
+    //   LOG_INFO("FW Version = %02u.%02u\n", (uint8_t)(version.fw >> 8), (uint8_t)(version.fw));
+    //   LOG_INFO("Type = %u\n", version.type);
   }
   PROCESS_END();
 }
